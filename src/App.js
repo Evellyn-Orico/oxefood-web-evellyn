2fce73003635eda20a1506b3bfd6011a43435e70
Aluno <aluno@ifpe.com>
2025-04-09 21:21:18 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import Home from './views/home/Home';
+
 
 function App() {
   return (
     <div className="App">
+
+      <Home />
+
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
