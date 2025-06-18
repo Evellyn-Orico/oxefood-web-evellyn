ce7bf68f7ce1fc1899459c43096855a82803faf8
Evellyn-Orico <116511322+Evellyn-Orico@users.noreply.github.com>
2025-06-18 19:42:38 +0000

    endereço

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 
 import FormCliente from './views/cliente/FormCliente';
+import FormEndereco from "./views/cliente/FormEndereço";
 import ListCliente from './views/cliente/ListCliente';
 import FormCupomDesconto from './views/cupomdesconto/FormCupomDesconto';
 import ListCupomDesconto from './views/cupomdesconto/ListCupomDesconto';
@@ -10,19 +11,21 @@ import Home from './views/home/Home';
 import FormProduto from './views/produto/FormProduto';
 import ListProduto from './views/produto/ListProduto';
 
-function Rotas() { 
+function Rotas() {
     return (
         <>
             <Routes>
-                <Route path="/" element={ <Home/> } /> 
-                <Route path="list-cliente" element={ <ListCliente/> } /> 
-                <Route path="list-produto" element={ <ListProduto/> } />
-                <Route path="list-entregador" element={ <ListEntregador/> } />
-                <Route path="list-cupomdesconto" element={ <ListCupomDesconto/> } />
-                <Route path="form-cliente" element={ <FormCliente/> } />
-                <Route path="form-produto" element={ <FormProduto/> } />
-                <Route path="form-entregador" element={ <FormEntregador/> } />
-                <Route path="form-cupomdesconto" element={ <FormCupomDesconto/> } />
+                <Route path="/" element={<Home />} />
+                <Route path="list-cliente" element={<ListCliente />} />
+                <Route path="list-produto" element={<ListProduto />} />
+                <Route path="list-entregador" element={<ListEntregador />} />
+                <Route path="list-cupomdesconto" element={<ListCupomDesconto />} />
+                <Route path="form-cliente" element={<FormCliente />} />
+                <Route path="form-produto" element={<FormProduto />} />
+                <Route path="form-entregador" element={<FormEntregador />} />
+                <Route path="form-cupomdesconto" element={<FormCupomDesconto />} />
+
+                <Route path="form-endereco/:idCliente/:idEndereco?" element={<FormEndereco />} />
 
             </Routes>
         </>
